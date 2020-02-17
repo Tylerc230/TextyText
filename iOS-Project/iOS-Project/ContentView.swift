d24d566c1507e0944d65ee00f1d4609f675ba3b3
Tyler Casselman <tylerc230@gmail.com>
2020-02-17 09:46:38 -0800

    Add example project

---
+++ iOS-Project/iOS-Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Project
+//
+//  Created by Tyler Casselman on 2/17/20.
+//  Copyright © 2020 13bit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
